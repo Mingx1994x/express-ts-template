c50289f0cf3214aa12cbfb78808346b5324bc84d
Mingx1994x <mggxmingx@gmail.com>
2026-01-19 11:51:55 +0800

    feat: 登入路由新增欄位驗證規則與驗證 middleware

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 import { checkout, login } from '../controllers/user.js';
 import { handleErrorAsync } from '../utils/handleError.js';
+import { loginValidator } from '../validators/user.validator.js';
+import { handleValidation } from '../middlewares/validateMiddleware.js';
 
 import type { Request, Response, NextFunction } from 'express';
 const router = express.Router();
@@ -11,11 +13,13 @@ router.get('/', function (req: Request, res: Response, next: NextFunction) {
   res.send('respond with a resource');
 });
 
-// 建立測試 userList API
-// router.get('/list', getUsersList);
-
 // 登入
-router.post('/login', handleErrorAsync(login));
+router.post(
+  '/login',
+  loginValidator,
+  handleValidation,
+  handleErrorAsync(login),
+);
 
 // 登入驗證
 router.post('/check', handleErrorAsync(checkout));
